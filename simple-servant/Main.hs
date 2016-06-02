98e62a7d5e97357546626797b6e138b1a0c1f84c
Sönke Hahn <soenkehahn@gmail.com>
2016-06-02 17:00:22 +0800

    code: included stubs to save time

--- simple-servant/Main.hs
+++ simple-servant/Main.hs
@@ -1,5 +1,39 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TypeOperators #-}
 
 module Main where
 
+import           Data.Aeson
+import           Data.Proxy
+import           GHC.Generics
+import           Network.Wai.Handler.Warp as Warp
+
+import           SimpleServant
+
 main :: IO ()
-main = return ()
+main = do
+  putStrLn $ docs myApi
+  Warp.run 8000 $ serve myApi server
+
+type MyApi =
+  Get Item
+
+myApi :: Proxy MyApi
+myApi = Proxy
+
+data Item
+  = Item {
+    itemId :: Integer,
+    itemData :: String
+  }
+  deriving (Generic)
+
+instance ToJSON Item
+
+mkExampleItem :: Integer -> Item
+mkExampleItem n =
+  Item n ("example item: " ++ show n)
+
+server :: Server MyApi
+server = NYI
--- simple-servant/SimpleServant.hs
+++ simple-servant/SimpleServant.hs
@@ -1,2 +1,97 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
 
 module SimpleServant where
+
+import           Data.Aeson
+import           Data.Proxy
+import           Data.String.Conversions
+import           Data.Typeable
+import           GHC.TypeLits
+import           Network.HTTP.Types
+import           Network.Wai
+
+data Get (a :: *)
+
+data (x :: k) :> api
+infixr 5 :>
+
+data a :<|> b
+  = a :<|> b
+infixr 4 :<|>
+
+data QueryParam (name :: Symbol) (param :: *)
+
+-- docs
+
+docs :: HasDocs api => Proxy api -> String
+docs = unlines . docsLines
+
+class HasDocs api where
+  docsLines :: Proxy api -> [String]
+
+instance
+  HasDocs (Get a) where
+
+  docsLines = error "NYI: Get"
+
+instance
+  HasDocs ((path :: Symbol) :> api) where
+
+  docsLines = error "NYI: paths"
+
+instance
+  HasDocs (a :<|> b) where
+
+  docsLines = error "NYI: :<|>"
+
+instance
+  HasDocs (QueryParam name param) where
+
+  docsLines = error "NYI: QueryParam"
+
+-- server
+
+serve :: HasServer api => Proxy api
+  -> Server api -> Application
+serve proxy server request send =
+  case route proxy server request of
+    Just mkResponse ->
+      mkResponse >>= send
+    Nothing ->
+      send $ responseLBS status500 [] "error!"
+
+class HasServer api where
+  type Server api :: *
+  route :: Proxy api -> Server api
+    -> Request -> Maybe (IO Response)
+
+data NYI = NYI
+
+instance
+  HasServer (Get a) where
+
+  type Server (Get a) = NYI
+  route = error "NYI: Get"
+
+instance
+  HasServer ((path :: Symbol) :> api) where
+
+  type Server (path :> api) = NYI
+  route = error "NYI: paths"
+
+instance
+  HasServer (a :<|> b) where
+
+  type Server (a :<|> b) = NYI
+  route = error "NYI: :<|>"
+
+instance
+  HasServer (QueryParam name param) where
+
+  type Server (QueryParam name param) = NYI
+  route = error "NYI: QueryParam"
